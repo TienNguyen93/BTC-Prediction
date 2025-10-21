edc4dc96e36719965f9359919b368f06cf32fcf6
TienNguyen93 <nguyentien9933@gmail.com>
2025-10-20 22:39:35 -0400

    modify dataset

--- models/tcn.py
+++ models/tcn.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.nn.utils import weight_norm
 
+
+# originally from https://github.com/locuslab/TCN/blob/master/TCN/tcn.py
 class Chomp1d(nn.Module):
     """
     Removes the last elements of a time series to ensure causal convolutions
@@ -16,12 +18,15 @@ class Chomp1d(nn.Module):
 class TemporalBlock(nn.Module):
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
+
+        # 1st layer
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                           stride=stride, padding=padding, dilation=dilation))
         self.chomp1 = Chomp1d(padding)
         self.relu1 = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout)
 
+        # 2nd layer
         self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
                                           stride=stride, padding=padding, dilation=dilation))
         self.chomp2 = Chomp1d(padding)
@@ -61,25 +66,31 @@ class TCN(nn.Module):
             dilation_size = 2 ** i
             in_channels = num_features if i == 0 else num_channels[i-1]
             out_channels = num_channels[i]
-            layers += [TemporalBlock(in_channels, out_channels, kernel_size,
-                                   stride=1, dilation=dilation_size,
-                                   padding=(kernel_size-1) * dilation_size,
-                                   dropout=dropout)]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                   padding=(kernel_size-1) * dilation_size, dropout=dropout)]
 
         self.network = nn.Sequential(*layers)
-        self.linear = nn.Linear(num_channels[-1], 1)
-        self.init_weights()
+        # self.linear = nn.Linear(num_channels[-1], 1)
+        # self.init_weights()
 
-    def init_weights(self):
-        self.linear.weight.data.normal_(0, 0.01)
+    # def init_weights(self):
+    #     self.linear.weight.data.normal_(0, 0.01)
 
     def forward(self, x):
+        """
+        previous code
+
         # x input shape: [batch_size, sequence_length, num_features]
         # Reshape for TCN: [batch_size, num_features, sequence_length]
         # x = x.transpose(1, 2)
-        print(f"Input shape before permute: {x.shape}")
-        
+        # print(f"Input shape before permute: {x.shape}")
+
         y = self.network(x)
         # Use the last timestep's features for prediction
         out = self.linear(y[:, :, -1])
         return out
+        """
+
+        # new code
+        return self.network(x)
+        
